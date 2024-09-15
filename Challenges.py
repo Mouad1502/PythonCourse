bd8657332d00744896f507c67cd6ffa7b14223f8
Mouad1502 <mrmouadabbar@gmail.com>
2024-09-15 14:40:31 +0100

    day2done

--- Challenges.py
+++ Challenges.py
@@ -1,7 +1,24 @@
 ## Day 1 Challenge
-print("Click Run to run the final project you will build")
-print("Welcome to the Band Name Generator.")
-CityName = input("What's the name of the city you grew up in?\n")
-PetName = input("What's the name of your pet's name ?\n")
-print("Your band name is : ", CityName, PetName)
+# print("Click Run to run the final project you will build")
+# print("Welcome to the Band Name Generator.")
+# CityName = input("What's the name of the city you grew up in?\n")
+# PetName = input("What's the name of your pet's name ?\n")
+# print("Your band name is : ", CityName, PetName)
+##################################################################
+## Day 2 Challenge
+# print("Welcome to the tip calculator:")
+# print("====================")
+# totalBill = float(input("What was the total bill?"))
+# tip = float(input("How much tip would you like to give? 10, 12 or 15?"))
+# numberPeople = int(input("How many people to split the bill?"))
+# if(tip == 10):
+#     print("Each person should pay:", round(totalBill*1.1/numberPeople,2))
+# elif(tip == 12):
+#     print("Each person should pay:", round(totalBill * 1.12 / numberPeople,2))
+# elif(tip == 15):
+#     print("Each person should pay:", round(totalBill * 1.15 / numberPeople,2))
+# else:
+#     print("the tip chosen is not taken by consideration.")
+#################################################################################
+## Day3 Challenge
 
