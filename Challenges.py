4e6c3e682a0056898fb440fa4d7d451ae98ba56c
Mouad1502 <mrmouadabbar@gmail.com>
2024-09-15 13:26:49 +0100

    day 2 challenge done

---
+++ Challenges.py
@@ -0,0 +1,7 @@
+## Day 1 Challenge
+print("Click Run to run the final project you will build")
+print("Welcome to the Band Name Generator.")
+CityName = input("What's the name of the city you grew up in?\n")
+PetName = input("What's the name of your pet's name ?\n")
+print("Your band name is : ", CityName, PetName)
+
