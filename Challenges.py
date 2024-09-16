97104420a3a45c376b71582c4527a4a3184d985e
Mouad1502 <mrmouadabbar@gmail.com>
2024-09-16 14:06:37 +0100

    day 3 done

--- Challenges.py
+++ Challenges.py
@@ -21,4 +21,26 @@
 #     print("the tip chosen is not taken by consideration.")
 #################################################################################
 ## Day3 Challenge
+# print("Welcome to Treasure Island.\nYour mission is to find the treasure.")
+# choice = input("You're at a cross road. Where do you want to go? Type 'left' or 'right' .")
+# if choice == 'left':
+#     print("You made the right choice, proceed to the next step!")
+# else:
+#     print("You fell into a hole, Game Over :(")
+# choice = input("Now you re in front of a swamp and something is going on inside the water make your choice : 'swim' or 'wait' .")
+# if choice == 'wait':
+#     print("You made the right choice, proceed to the next step!")
+# else:
+#     print("You got attacked by trout, Game Over :(")
+# choice = input("A path is gettting carved in the middle of the swamp, now you have 3 doors in front of you each one has its distinctive colour, choose which door you re going to go through: 'blue', 'red' or 'yellow' .")
+# if choice == 'blue':
+#     print("You got eaten by beasts, Game Over :(")
+# elif choice == 'red':
+#     print("You got burned by fire, Game Over :(")
+# elif choice == 'yellow':
+#     print("Congratulations you survived the game of fate!!")
+# else:
+#     print("Game over you can't even choose the right color :/ !")
+##############################################################################
+## Day 4 Challenge
 
